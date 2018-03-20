d03d8eb54c4ebe1326570c9de8269a3575a9eebd
James McClain <jmcclain@azavea.com>
2018-03-20 17:17:45 -0400

    Simple Node Query

--- ad/src/main/scala/AugmentedDiff.scala
+++ ad/src/main/scala/AugmentedDiff.scala
@@ -7,11 +7,68 @@ import org.apache.spark.sql.functions._
 
 import osmdiff.common.Common
 
+import scala.collection.mutable
+
 
 object AugmentedDiff {
 
+  val spark = Common.sparkSession("Augmented Diff")
+  import spark.implicits._
+
+  val nodeToWays = spark.table("node_to_ways")
+  val wayToRelations = spark.table("way_to_relations")
+  val relationToRelations = spark.table("relation_to_relations")
+
+  private def recurseNode(nodeId: Long) = {
+    var keepGoing = true
+    val relations = mutable.Set.empty[Long]
+
+    val ways = nodeToWays
+      .filter(col("id") === nodeId)
+      .map({ r => r.getAs[Long]("way_id") })
+      .collect
+      .toSet
+
+    relations ++=
+    (if (ways.isEmpty)
+      Set.empty[Long]
+    else {
+      wayToRelations
+        .filter(col("id") >= ways.min)
+        .filter(col("id") <= ways.max)
+        .filter({ r => ways.contains(r.getAs[Long]("id")) })
+        .map({ r => r.getAs[Long]("relation_id") })
+        .collect
+        .toSet
+    })
+
+    while (keepGoing) {
+      keepGoing = false
+      val newRelations =
+        if (relations.isEmpty) Set.empty[Long]
+        else {
+          relationToRelations
+            .filter(col("id") >= relations.min)
+            .filter(col("id") <= relations.max)
+            .filter({ r => relations.contains(r.getAs[Long]("id")) })
+            .map({ r => r.getAs[Long]("relation_id") })
+            .collect
+            .toSet
+        }
+      if (!newRelations.subsetOf(relations)) {
+        keepGoing = true
+        relations ++= newRelations
+      }
+    }
+
+    (ways, relations)
+  }
+
   def main(args: Array[String]): Unit = {
-    val spark = Common.sparkSession("Augmented Diff")
+    val nodeId = args(0).toLong
+    val (ways, relations) = recurseNode(nodeId)
+
+    println(s"node=$nodeId ways=$ways relations=$relations")
   }
 
 }
--- indexer/src/main/scala/Indexer.scala
+++ indexer/src/main/scala/Indexer.scala
@@ -14,29 +14,32 @@ object Indexer {
   def main(args: Array[String]): Unit = {
     val spark = Common.sparkSession("Indexer")
 
-    val df: DataFrame = spark.read.orc(args(0))
-    val nodeToWays: DataFrame = df
+    val osm = spark.read.orc(args(0))
+    val nodeToWays = osm
       .filter(col("type") === "way")
       .select(explode(col("nds.ref")).as("id"), col("timestamp").as("valid_from"), col("id").as("way_id"))
       .withColumn("valid_until", Common.farFuture())
       .withColumn("idstamp", Common.idTimestamp(col("id"), col("valid_from")))
-    val xToRelations: DataFrame = df
+    val xToRelations = osm
       .filter(col("type") === "relation")
       .select(explode(col("members")).as("id"), col("timestamp").as("valid_from"), col("id").as("relation_id"))
-    val wayToRelations: DataFrame = xToRelations
+    val wayToRelations = xToRelations
       .filter(col("id.type") === "way")
       .select(col("id.ref").as("id"), col("valid_from"), col("relation_id"))
       .withColumn("valid_until", Common.farFuture())
       .withColumn("idstamp", Common.idTimestamp(col("id"), col("valid_from")))
-    val relationToRelations: DataFrame  = xToRelations
+    val relationToRelations  = xToRelations
       .filter(col("id.type") === "relation")
       .select(col("id.ref").as("id"), col("valid_from"), col("relation_id"))
       .withColumn("valid_until", Common.farFuture())
       .withColumn("idstamp", Common.idTimestamp(col("id"), col("valid_from")))
 
-    nodeToWays.printSchema
-    wayToRelations.printSchema
-    relationToRelations.printSchema
+    osm
+      .write
+      .mode("overwrite")
+      .format("orc")
+      .sortBy("id").bucketBy(8, "id").partitionBy("type")
+      .saveAsTable("osm")
 
     nodeToWays
       .write

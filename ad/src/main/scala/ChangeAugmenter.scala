f2ac1a441a890755f705c077b9f2fb898abebb0e
James McClain <jmcclain@azavea.com>
2018-03-28 12:44:32 -0400

    OSM Schema

--- ad/src/main/scala/ChangeAugmenter.scala
+++ ad/src/main/scala/ChangeAugmenter.scala
@@ -1,10 +1,35 @@
 package osmdiff
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
 
 import org.openstreetmap.osmosis.core.container.v0_6._
 import org.openstreetmap.osmosis.core.task.v0_6.ChangeSink
 
+
+object ChangeAugmenter {
+
+  val ndsSchema = ArrayType(StructType(List(StructField("ref", LongType, true))))
+  val membersSchema = ArrayType(StructType(List(
+    StructField("type", StringType, true),
+    StructField("ref", LongType, true),
+    StructField("role", StringType, true))))
+  val osmSchema = List(
+    StructField("id", LongType, true),
+    StructField("tags", MapType(StringType, StringType), true),
+    StructField("lat", DecimalType(9, 7), true),
+    StructField("lon", DecimalType(10, 7), true),
+    StructField("nds", ndsSchema, true),
+    StructField("members", membersSchema, true),
+    StructField("changeset", LongType, true),
+    StructField("timestamp", TimestampType, true),
+    StructField("uid", LongType, true),
+    StructField("user", StringType, true),
+    StructField("version", LongType, true),
+    StructField("visible", BooleanType, true),
+    StructField("type", StringType, true)
+  )
+}
 
 class ChangeAugmenter(spark: SparkSession) extends ChangeSink {
   var counter = 0L

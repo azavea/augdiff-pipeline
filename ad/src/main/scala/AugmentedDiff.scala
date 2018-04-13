2e1ddfec610060ca66756617412e7f079a3f4ba0
James McClain <jmcclain@azavea.com>
2018-04-13 15:24:46 -0400

    Reimplement Query

--- ad/src/main/scala/AugmentedDiff.scala
+++ ad/src/main/scala/AugmentedDiff.scala
@@ -20,10 +20,46 @@ object AugmentedDiff {
   val spark = Common.sparkSession("Augmented Diff")
   import spark.implicits._
 
-  val window1 = Window.partitionBy("prior_id", "prior_type").orderBy(desc("instant"))
-  val window2 = Window.partitionBy("id", "type").orderBy(desc("timestamp"))
+  val window1 = Window.partitionBy("left.a", "left.b").orderBy(desc("instant"))
+  val window2 = Window.partitionBy("left.id", "left.type").orderBy(desc("timestamp"))
 
-  def augment(rows: DataFrame) = ???
+  def augment(rows: DataFrame) = { // XXX only works in the present
+    val index = spark.table("index").select(Common.edgeColumns: _*) // search index
+      .union(spark.table("index_updates").select(Common.edgeColumns: _*))
+    val osm = spark.table("osm").select(Common.osmColumns: _*) // OSM bulk
+      .union(spark.table("osm_updates").select(Common.osmColumns: _*))
+    val pointers = index.as("left") // Use the index to get entities
+                                    // touched by the rows, and all
+                                    // entities that those entities
+                                    // depend on
+      .join(
+        rows.as("right"),
+        ((col("left.bp") === col("right.p")) && // Partition pruning
+         (col("left.b.id") === col("right.id")) && // Same id
+         (col("left.b.type") === col("right.type"))), // Same type
+        "left_semi")
+      .withColumn("rank", rank().over(window1))
+      .filter(col("rank") === 1)
+      .select(
+        col("left.a.id").as("id"),
+        col("left.a.type").as("type"),
+        col("left.ap").as("p"))
+      .union(rows.select(
+        col("id").as("id"),
+        col("type").as("type"),
+        col("p").as("p")))
+
+    osm.as("left")
+      .join(
+      pointers.as("right"),
+        ((col("left.p") === col("right.p")) && // Partition pruning
+         (col("left.id") === col("right.id")) && // Same id
+         (col("left.type") === col("right.type"))), // Same type
+        "left_semi")
+      .withColumn("rank", rank().over(window2))
+      .filter(col("rank") === 1)
+      .select(Common.osmColumns: _*)
+  }
 
   def main(args: Array[String]): Unit = {
 
@@ -41,9 +77,16 @@ object AugmentedDiff {
       val time1 = System.currentTimeMillis
       println(s"size: ${augment(updates).count}")
       val time2 = System.currentTimeMillis
-      println(s"size: ${augment(updates).count}")
+      val augmented = augment(updates)
+      println(s"size: ${augmented.count}")
       val time3 = System.currentTimeMillis
       println(s"times: ${time2 - time1} ${time3 - time2}")
+
+      if ((args.length > 1) && (args(1) == "yyy")) {
+        updates.select(Common.osmColumns: _*).foreach({ row => println(s"◯ $row") })
+        println
+        augmented.select(Common.osmColumns: _*).foreach({ row => println(s"◯ $row") })
+      }
     }
   }
 

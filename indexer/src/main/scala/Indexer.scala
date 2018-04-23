e4dad02448e6d5427e8f46388d1924e5879f7274
James McClain <jmcclain@azavea.com>
2018-04-23 12:03:21 -0400

    Use `decline` For Command Line Arguments

--- indexer/src/main/scala/Indexer.scala
+++ indexer/src/main/scala/Indexer.scala
@@ -7,46 +7,51 @@ import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import org.apache.spark.storage.StorageLevel
 
+import cats.implicits._
+import com.monovore.decline._
 
-object Indexer {
 
-  def main(args: Array[String]): Unit = {
+object Indexer extends CommandApp(
+  name = "OSM Indexer",
+  header = "Index OSM",
+  main = {
     val spark = Common.sparkSession("Indexer")
     import spark.implicits._
 
     Common.denoise
 
-    val osm = spark
-      .read.orc(args(0))
-      .withColumn("p", Common.partitionNumberUdf(col("id"), col("type")))
-      .select(Common.osmColumns: _*)
-    val persistence = if (args.length > 1) {
-      args(1) match {
-        case "memory_only" | "MEMORY_ONLY" => Some(StorageLevel.MEMORY_ONLY)
-        case "memory_and_disk" | "MEMORY_AND_DISK" => Some(StorageLevel.MEMORY_AND_DISK)
-        case "memory_only_ser" | "MEMORY_ONLY_SER" => Some(StorageLevel.MEMORY_ONLY_SER)
-        case "memory_and_disk_ser" | "MEMORY_AND_DISK_SER" => Some(StorageLevel.MEMORY_AND_DISK_SER)
-        case "disk_only" | "DISK_ONLY"=> Some(StorageLevel.DISK_ONLY)
-        case "off_heap" | "OFF_HEAP" => Some(StorageLevel.OFF_HEAP)
+    val orcfile =
+      Opts.option[String]("orcfile", help = "ORC file containing OSM data")
+    val partitions =
+      Opts.option[Int]("partitions", help = "Number of partitions to use").orNone
+    val persistence =
+      Opts.option[String]("persistence", help = "Storage-level to use during indexing").orNone
+
+    (orcfile, partitions, persistence).mapN({ (orcfile, partitions, _persistence) =>
+      val persistence = _persistence match {
+        case Some("memory_only") | Some("MEMORY_ONLY") => Some(StorageLevel.MEMORY_ONLY)
+        case Some("memory_and_disk") | Some("MEMORY_AND_DISK") => Some(StorageLevel.MEMORY_AND_DISK)
+        case Some("memory_only_ser") | Some("MEMORY_ONLY_SER") => Some(StorageLevel.MEMORY_ONLY_SER)
+        case Some("memory_and_disk_ser") | Some("MEMORY_AND_DISK_SER") => Some(StorageLevel.MEMORY_AND_DISK_SER)
+        case Some("disk_only") | Some("DISK_ONLY") => Some(StorageLevel.DISK_ONLY)
+        case Some("off_heap") | Some("OFF_HEAP") => Some(StorageLevel.OFF_HEAP)
         case _ => None
       }
-    }
-    else None
 
-    val partitions =
-      if (args.length > 2) Some(args(2).toInt)
-      else None
-    val index = ComputeIndex(osm, persistence, partitions)
+      val osm = spark
+        .read.orc(orcfile)
+        .withColumn("p", Common.partitionNumberUdf(col("id"), col("type")))
+        .select(Common.osmColumns: _*)
+      val index = ComputeIndex(osm, persistence, partitions)
+      partitions match {
+        case Some(p) => Common.saveIndex(index.repartition(p), "index", "overwrite")
+        case None => Common.saveIndex(index, "index", "overwrite")
+      }
+      partitions match {
+        case Some(p) => Common.saveBulk(osm.repartition(p), "osm", "overwrite")
+        case None => Common.saveBulk(osm, "osm", "overwrite")
+      }
 
-    if (args.length > 2) {
-      val p = args(2).toInt
-      Common.saveIndex(index.repartition(p), "index", "overwrite")
-      Common.saveBulk(osm.repartition(p), "osm", "overwrite")
-    } else {
-      Common.saveIndex(index, "index", "overwrite")
-      Common.saveBulk(osm, "osm", "overwrite")
-    }
-
+    })
   }
-
-}
+)

012d1b917e3935767cb984cb35788f5be5eb205b
James McClain <jmcclain@azavea.com>
2018-06-10 15:38:35 -0400

    `members`

--- common/src/main/scala/OrcBackend.scala
+++ common/src/main/scala/OrcBackend.scala
@@ -33,7 +33,7 @@ object OrcBackend {
 
     // https://orc.apache.org/docs/core-java.html
     while(rows.nextBatch(batch)) {
-      val ids = batch.cols(0).asInstanceOf[vector.LongColumnVector] // XXX p column dropped (due to partitioned write?)
+      val ids = batch.cols(0).asInstanceOf[vector.LongColumnVector] // id at 0 because p column dropped due to partitioned write
       val types = batch.cols(1).asInstanceOf[vector.BytesColumnVector]
       val tagss = batch.cols(2).asInstanceOf[vector.MapColumnVector]
       val tagKeys = tagss.keys.asInstanceOf[vector.BytesColumnVector]
@@ -43,18 +43,21 @@ object OrcBackend {
       val ndss = batch.cols(5).asInstanceOf[vector.ListColumnVector]
       val ndssField = ndss.child.asInstanceOf[vector.StructColumnVector].fields(0).asInstanceOf[vector.LongColumnVector]
       val memberss = batch.cols(6).asInstanceOf[vector.ListColumnVector]
+      val memberssChild = memberss.child.asInstanceOf[vector.StructColumnVector]
+      val memberssTypes = memberssChild.fields(0).asInstanceOf[vector.BytesColumnVector]
+      val memberssRefs = memberssChild.fields(1).asInstanceOf[vector.LongColumnVector]
+      val memberssRoles = memberssChild.fields(2).asInstanceOf[vector.BytesColumnVector]
 
       Range(0, batch.size).foreach({ i =>
-        val idIndex = if (ids.isRepeating) 0; else i
-        val typeIndex = if (types.isRepeating) 0; else i
-        val tagsIndex = if (tagss.isRepeating) 0; else i
-        val latIndex = if (lats.isRepeating) 0; else i
-        val lonIndex = if (lons.isRepeating) 0; else i
-        val ndsIndex = if (ndss.isRepeating) 0; else i
 
+        // id
+        val idIndex = if (ids.isRepeating) 0; else i
         val id: Long =
           if (ids.noNulls || !ids.isNull(idIndex)) ids.vector(idIndex)
           else -1
+
+        // type
+        val typeIndex = if (types.isRepeating) 0; else i
         val tipe: String =
           if (types.noNulls || !types.isNull(typeIndex)) {
             val start = types.start(typeIndex)
@@ -62,6 +65,9 @@ object OrcBackend {
             types.vector(typeIndex).drop(start).take(length).map(_.toChar).mkString
           }
           else null
+
+        // tags
+        val tagsIndex = if (tagss.isRepeating) 0; else i
         val tags: Map[String, String] =
           if (tagss.noNulls || !tagss.isNull(tagsIndex)) {
             val offset = tagss.offsets(tagsIndex).toInt
@@ -69,20 +75,37 @@ object OrcBackend {
             Range(offset, offset+length).map({ j =>
               val keyIndex = if (tagKeys.isRepeating) 0; else j
               val valueIndex = if (tagValues.isRepeating) 0; else j
-              val key: String = tagKeys.vector(keyIndex).drop(tagKeys.start(keyIndex)).take(tagKeys.length(keyIndex)).map(_.toChar).mkString
-              val value: String = tagValues.vector(valueIndex).drop(tagValues.start(valueIndex)).take(tagValues.length(valueIndex)).map(_.toChar).mkString
+              val key: String = tagKeys
+                .vector(keyIndex)
+                .drop(tagKeys.start(keyIndex))
+                .take(tagKeys.length(keyIndex))
+                .map(_.toChar).mkString
+              val value: String = tagValues
+                .vector(valueIndex)
+                .drop(tagValues.start(valueIndex))
+                .take(tagValues.length(valueIndex))
+                .map(_.toChar).mkString
               key -> value
             }).toMap
           }
           else null
+
+        // lat
+        val latIndex = if (lats.isRepeating) 0; else i
         val lat: java.math.BigDecimal =
           if (lats.noNulls || !lats.isNull(latIndex))
             lats.vector(latIndex).getHiveDecimal.bigDecimalValue
           else null
+
+        // lon
+        val lonIndex = if (lons.isRepeating) 0; else i
         val lon: java.math.BigDecimal =
           if (lons.noNulls || !lons.isNull(lonIndex))
             lons.vector(lonIndex).getHiveDecimal.bigDecimalValue
           else null
+
+        // nds
+        val ndsIndex = if (ndss.isRepeating) 0; else i
         val nds: Array[Long] =
           if (ndss.noNulls || !ndss.isNull(ndsIndex)) {
             val offset = ndss.offsets(ndsIndex).toInt
@@ -94,14 +117,44 @@ object OrcBackend {
           }
           else Array.empty[Long]
 
+        // members
+        val membersIndex = if (memberss.isRepeating) 0; else i
+        val members: Array[Row] =
+          if (memberss.noNulls || !memberss.isNull(membersIndex)) {
+            val offset = memberss.offsets(membersIndex).toInt
+            val length = memberss.lengths(membersIndex).toInt
+            Range(offset, offset+length).toArray.map({ j =>
+              val typeIndex = if (memberssTypes.isRepeating) 0; else j
+              val refIndex = if (memberssRefs.isRepeating) 0; else j
+              val roleIndex = if (memberssRoles.isRepeating) 0; else j
+              val tipe2: String = memberssTypes
+                .vector(typeIndex)
+                .drop(memberssTypes.start(typeIndex))
+                .take(memberssTypes.length(typeIndex))
+                .map(_.toChar).mkString
+              val ref: Long = memberssRefs.vector(refIndex)
+              val role: String = memberssRoles
+                .vector(roleIndex)
+                .drop(memberssRoles.start(roleIndex))
+                .take(memberssTypes.length(roleIndex))
+                .map(_.toChar).mkString
+              Row(tipe2, ref, role)
+            })
+          }
+          else Array.empty[Row]
+
         val pair = (id, tipe)
         if (pairs.contains(pair)) {
           if (tipe == "node") {
-            // println(Row(id, tipe, tags, lat, lon, null))
+            println(Row(id, tipe, tags, lat, lon, nds, members))
           }
           else if (tipe == "way") {
-            println(Row(id, tipe, tags, null, null, nds))
+            println(Row(id, tipe, tags, null, null, nds, members))
             println(nds.toList)
+          }
+          else if (tipe == "relation") {
+            println(Row(id, tipe, tags, null, null, nds, members))
+            println(members.toList)
           }
         }
       })
